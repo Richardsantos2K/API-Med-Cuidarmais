a029ca6a3fec08e1db5c41f28da6bf2eb76b00e2
Richardsantos2K <richarddossantos2022@gmail.com>
2023-10-24 10:02:40 -0300

    API Cuida+ alterações na API, Listagem dia 24/10/2023

--- api/src/main/java/com/med/Cuidarmais/api/controller/MedicoController.java
+++ api/src/main/java/com/med/Cuidarmais/api/controller/MedicoController.java
@@ -1,48 +1,34 @@
 package com.med.Cuidarmais.api.controller;
 
 import com.med.Cuidarmais.api.medico.DadosCadastroMed;
+import com.med.Cuidarmais.api.medico.DadosListagemMedico;
 import com.med.Cuidarmais.api.medico.Medico;
 import com.med.Cuidarmais.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-
-    /*pegaremos o repositoty e mandaremos percistir no banco de dados.
-
-      precisamos da classe da interface repository.
-
-      e por isso declaramos como sendo um atributo da classe controller.
-
-      Com isso faremos com que ao insves de nos instanciarmos esta classe
-      o proprio spring ira instanciar para gente.
-
-      Isso se chama INJEÇÃO DE DEPENDÊNCIAS
-     */
     @Autowired // feito isso ele ira saber que é ele que ira instanciar e passar este atributo repository dentro da nossa classe controller
     private MedicoRepository repository;
 
     @PostMapping
-    @Transactional/*@Transactional é uma anotação em Java usada para marcar métodos ou classes,
-                    indicando que as operações dentro delas são transacionais, ou seja, elas serão executadas como uma única unidade de trabalho
-                     que é confirmada (commit) ou revertida (rollback) automaticamente se ocorrer algum erro.
-                     Isso é comumente usado em acesso a bancos de dados para garantir a consistência dos dados.*/
+    @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMed dados){
 
         repository.save(new Medico(dados));
-    } /*
-    Essa linha está salvando uma nova instância de Medico no repositório (em um banco de dados)
-     usando o método save do objeto repository. ou seja, sempre que tiver um novo medico passara aqui para salvar.
-     A instância de Medico é criada com base nos dados fornecidos no parâmetro dados
-      e passada diretamente para o repositório para ser persistida.
-      Isso é uma operação comum ao usar frameworks de persistência de dados, como o Spring Data JPA,
-      para armazenar informações em um banco de dados.
-    */
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){//Pageable interface do proprio spring
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+    }
+
 }
